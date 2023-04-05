e72eff7ed44b21e7c39cbe0d36cba2f1cc1ade96
mbr1337 <mb651424@gmail.com>
2023-04-05 21:50:08 +0200

    2nd api testing

--- src/components/waltuh.jsx
+++ src/components/waltuh.jsx
@@ -8,10 +8,13 @@ function Waltuh() {
     const [timeArray, setTimeArray] = useState([]);
     const [temperatureArray, setTemperatureArray] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const today = getCurrentDate('-');
-    const previousWeekDate = getPreviousWeekDate('-');
+    const today = getCurrentDate('-',0);
+    const previousWeekDate = getPreviousWeekDate('-',0);
+    const previousWeekDateMinusOne = getPreviousWeekDate('-',1);
     const meteoWeatherURL = `https://archive-api.open-meteo.com/v1/archive?latitude=50.01&longitude=20.99&start_date=${previousWeekDate}&end_date=${previousWeekDate}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin`;
-
+    const weatherBitURL = `https://api.weatherbit.io/v2.0/history/hourly?lat=50.01&lon=20.99&start_date=${previousWeekDateMinusOne}&end_date=${previousWeekDate}&key=be3b07c2cf8742a992f1c40367ea1348`;
+    // https://www.weatherbit.io/history/hourly
+    // https://www.weatherbit.io/api
     useEffect(() => {
         axios.get(meteoWeatherURL).then((response) => {
             const hourly = response.data.hourly;
@@ -22,7 +25,12 @@ function Waltuh() {
             .catch(error => console.error(error));
     }, []);
 
-
+    useEffect(() => {
+        axios.get(weatherBitURL).then((response) => {
+            console.log(response.data);
+        })
+            .catch(error => console.error(error));
+    }, []);
 
     return (
         <div className="mainContainer">
--- src/utils/getDates.js
+++ src/utils/getDates.js
@@ -8,13 +8,13 @@ export function getCurrentDate(separator = '') {
     return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date < 10 ? `0${date}` : `${date}`}`
 }
 
-export function getPreviousWeekDate(separator = '') {
+export function getPreviousWeekDate(separator = '', numDays) {
     let newDate = new Date();
-    newDate.setDate(newDate.getDate() - 7);
-  
+    newDate.setDate(newDate.getDate() - (7 + numDays));
+
     let date = newDate.getDate();
     let month = newDate.getMonth() + 1;
     let year = newDate.getFullYear();
-  
+
     return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date < 10 ? `0${date}` : `${date}`}`;
-  }
+}
