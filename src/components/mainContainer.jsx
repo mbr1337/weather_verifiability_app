606ddd07d86213d9d0e36eb9c05c77923b3a2fb0
mbr1337 <mb651424@gmail.com>
2023-04-19 20:39:17 +0200

    testing recharts

---
+++ src/components/mainContainer.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import MeteoWeather from "./meteoWeather";
+import ApiWeather from "./apiWeather";
+import VisualCrossing from "./visualCrossing";
+
+function MainContainer() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [updatedMeteoWeatherArray, setUpdatedMeteoWeatherArray] = useState([]);
+    const [updatedWeatherApiArray, setUpdatedWeatherApiArray] = useState([]);
+    const [updatedVisualCrossingArray, setUpdatedVisualCrossingArray] = useState([]);
+
+    function handleArrayUpdate(updatedArray, updateFunction) {
+        updateFunction(updatedArray);
+    }
+
+    return (
+        <div className="mainContainer">
+            {/* zmienic isloading na true */}
+            {isLoading ? <p>Loading...</p> : (
+                <div id="weathers">
+                    <MeteoWeather onArrayUpdate={(updatedArray) => handleArrayUpdate(updatedArray, setUpdatedMeteoWeatherArray)} />
+                    <ApiWeather onArrayUpdate={(updatedArray) => handleArrayUpdate(updatedArray, setUpdatedWeatherApiArray)} />
+                    <VisualCrossing onArrayUpdate={(updatedArray) => handleArrayUpdate(updatedArray, setUpdatedVisualCrossingArray)} />
+                </div>
+
+            )}
+        </div>
+    );
+}
+
+export default MainContainer;
